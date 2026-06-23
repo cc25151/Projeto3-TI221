7aa82701985e10ee1efb9ffb5601041ffb235c4f
Sakamoto <cc25151@g.unicamp.br>
2026-06-23 13:42:20 -0300

    inicial

---
+++ PilhaVetor.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class PilhaVetor<T> : IStack<T>
+{
+    const int MAXIMO = 500;
+    private int tamanhoFisico;
+    private T[] p;
+    private int topo;
+    public PilhaVetor() : this(MAXIMO) { }
+
+    public PilhaVetor(int quantasPosicoes)
+    {
+        tamanhoFisico = quantasPosicoes;
+        p = new T[quantasPosicoes];
+        topo = -1;
+    }
+
+    public int Tamanho => topo+1;
+
+    public bool EstaVazia => topo < 0;
+
+    public List<T> Conteudo()
+    {
+        List<T> resultado = new List<T>();
+        for (int indice = topo; indice >= 0; indice--)
+            resultado.Add(p[indice]);
+        return resultado;
+    }
+
+    public T Desempilhar()
+    {
+        if (EstaVazia)
+            throw new Exception("Pilha esvaziou (underflow)");
+        T dadoDoTopo = p[topo];
+        p[topo--] = default(T);
+        return dadoDoTopo;
+    }
+
+    public void Empilhar(T item)
+    {
+        if (Tamanho == tamanhoFisico)
+            throw new Exception("Pilha encheu (overflow)!");
+
+        p[++topo] = item;
+    }
+
+    public T OTopo()
+    {
+        if (EstaVazia)
+            throw new Exception("Pilha esvaziou (underflow)");
+        return p[topo];
+    }
+}
---
+++ apCalculadora/apCalculadora/Form1.cs
@@ -0,0 +1,10 @@
+namespace apCalculadora
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
